92734635e8ce0d368b751e2c87579a2227e1bcbf
Renato <renatoperezc@gmail.com>
2023-01-09 13:47:18 -0600

    Set storybook server and migrate to webpack5 builder

---
+++ packages/storybook/.storybook/main.js
@@ -0,0 +1,16 @@
+module.exports = {
+  stories: ['../stories/**/*.stories.mdx', '../stories/**/*.stories.@(js|jsx|ts|tsx)'],
+  addons: [
+    '@storybook/addon-a11y',
+    '@storybook/addon-links',
+    '@storybook/addon-essentials',
+    '@storybook/addon-interactions',
+  ],
+  framework: '@storybook/react',
+  docs: {
+    autodocs: true,
+  },
+  core: {
+    builder: 'webpack5',
+  },
+};
